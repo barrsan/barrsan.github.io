8503f6e16f6f13a4bd762d91cac839b630f9b837
Alex Baretsky <41ax@mail.ru>
2020-10-31 00:08:49 +0400

    Fix mobile scroll lags

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "277e978a921b32cf45e3";
+/******/ 	var hotCurrentHash = "958c17580f8d3919b567";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
