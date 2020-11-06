43da0c922b628e23f8885e2604914947537913b5
Alex Baretsky <41ax@mail.ru>
2020-11-07 03:17:19 +0400

    Update site

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b8b72230f01f0ae0af36";
+/******/ 	var hotCurrentHash = "6606c3ac6dcb3688eee9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
